2cf0796f8e33d76586ff853e39821773d36d589d
le0n229 <leon229@yandex.ru>
2019-04-24 14:06:12 +0300

    lesson6 base added

--- data/mygallery.sql
+++ data/mygallery.sql
@@ -1,10 +1,10 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.3
+-- version 4.8.5
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1:3306
--- Время создания: Апр 19 2019 г., 21:32
--- Версия сервера: 5.7.23
+-- Время создания: Апр 24 2019 г., 14:02
+-- Версия сервера: 5.7.25
 -- Версия PHP: 7.1.22
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
@@ -19,7 +19,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- База данных: `geek`
+-- База данных: `mygallery`
 --
 
 -- --------------------------------------------------------
@@ -44,22 +44,25 @@ INSERT INTO `category` (`id`, `category`) VALUES
 -- --------------------------------------------------------
 
 --
--- Структура таблицы `feedback`
+-- Структура таблицы `goods`
 --
 
-CREATE TABLE `feedback` (
+CREATE TABLE `goods` (
   `id` int(11) NOT NULL,
-  `name` text NOT NULL,
-  `feedback` text NOT NULL
+  `name` varchar(255) NOT NULL,
+  `price` float NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `picture` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Дамп данных таблицы `feedback`
+-- Дамп данных таблицы `goods`
 --
 
-INSERT INTO `feedback` (`id`, `name`, `feedback`) VALUES
-(9, 'Вася', 'Привет'),
-(11, 'Вася', 'Еще отзыв');
+INSERT INTO `goods` (`id`, `name`, `price`, `description`, `picture`) VALUES
+(1, 'Товар1', 10, 'Супер товар', '01.jpg'),
+(2, 'Товар2', 10.3, 'Супер товар', '02.jpg'),
+(3, 'Товар3', 9.4, 'Супер товар', '03.jpg');
 
 -- --------------------------------------------------------
 
@@ -82,6 +85,60 @@ INSERT INTO `news` (`id`, `category`, `prev`, `text`) VALUES
 (1, 2, 'В штабе Зеленского не признают референдум в Крыму', 'КИЕВ, 15 апр — РИА Новости. Пресс-служба кандидата в президенты Украины Владимира Зеленского заявила, что в его штабе не признают референдум о воссоединении Крыма с Россией.\r\n\"Так называемый \"референдум\" не может считаться актом, свидетельствующим о свободном волеизъявлении жителей Крыма\", — говорится в заявлении, которое имеется в распоряжении РИА Новости.'),
 (2, 2, 'Путин подписал закон о запрете размещения хостелов в жилых домах', 'МОСКВА, 15 апр - РИА Новости. Владимир Путин подписал закон о запрете размещения хостелов в многоквартирных домах с первого октября 2019 года, соответствующий документ опубликован на официальном портале правовой информации.\r\nЗакон запрещает использовать жилые помещения в качестве гостиницы или другого средства временного размещения. Предусматривается, что оказывать гостиничные услуги можно лишь после перевода жилого помещения в нежилое и оснащения его оборудованием надлежащего качества:');
 
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `pictures`
+--
+
+CREATE TABLE `pictures` (
+  `id` int(11) NOT NULL,
+  `path` varchar(11) NOT NULL,
+  `views` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `pictures`
+--
+
+INSERT INTO `pictures` (`id`, `path`, `views`) VALUES
+(1, '01.jpg', 4),
+(2, '02.jpg', 8),
+(4, '03.jpg', 1),
+(5, '04.jpg', 1),
+(6, '05.jpg', 3),
+(7, '06.jpg', 1),
+(8, '07.jpg', 2),
+(9, '08.jpg', 1),
+(10, '09.jpg', 1),
+(11, '10.jpg', 1),
+(12, '11.jpg', 1),
+(13, '12.jpg', 2),
+(14, '13.jpg', 1),
+(15, '14.jpg', 1),
+(16, '15.jpg', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `product_feedback`
+--
+
+CREATE TABLE `product_feedback` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `text` varchar(255) NOT NULL,
+  `product_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `product_feedback`
+--
+
+INSERT INTO `product_feedback` (`id`, `name`, `text`, `product_id`) VALUES
+(9, 'Михаил', '2', 2),
+(10, 'Михаил', '3', 3);
+
 --
 -- Индексы сохранённых таблиц
 --
@@ -93,15 +150,27 @@ ALTER TABLE `category`
   ADD PRIMARY KEY (`id`);
 
 --
--- Индексы таблицы `feedback`
+-- Индексы таблицы `goods`
 --
-ALTER TABLE `feedback`
+ALTER TABLE `goods`
   ADD PRIMARY KEY (`id`);
 
 --
 -- Индексы таблицы `news`
 --
 ALTER TABLE `news`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `pictures`
+--
+ALTER TABLE `pictures`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `product_feedback`
+--
+ALTER TABLE `product_feedback`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -115,16 +184,28 @@ ALTER TABLE `category`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
--- AUTO_INCREMENT для таблицы `feedback`
+-- AUTO_INCREMENT для таблицы `goods`
 --
-ALTER TABLE `feedback`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+ALTER TABLE `goods`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT для таблицы `news`
 --
 ALTER TABLE `news`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT для таблицы `pictures`
+--
+ALTER TABLE `pictures`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+
+--
+-- AUTO_INCREMENT для таблицы `product_feedback`
+--
+ALTER TABLE `product_feedback`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
